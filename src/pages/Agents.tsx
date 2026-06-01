2046d0cf76d66de8903421507cd9aa429a1dcd1e
digiwebdex <bditengineer@gmail.com>
2026-06-02 00:41:47 +0600

    Pass agents permission module into generic CRUD page

--- src/pages/Agents.tsx
+++ src/pages/Agents.tsx
@@ -2,5 +2,5 @@ import CrudPage from "@/components/CrudPage";
 import { UserCog } from "lucide-react";
 import { agentApi } from "@/lib/api";
 
-const Agents = () => <CrudPage title="Agents" icon={UserCog} api={agentApi} />;
+const Agents = () => <CrudPage title="Agents" icon={UserCog} api={agentApi} module="agents" />;
 export default Agents;
